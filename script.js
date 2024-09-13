bf0e3c5732966b0f54b534052dae17e3207c803e
AeswellClinic <clinicaeswell@gmail.com>
2024-09-13 12:34:16 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+    document.addEventListener('mouseover', function() {
+      const button = document.querySelector('.google-button');
+      button.classList.add('active');
+    });
+
+  
+    document.addEventListener('mouseout', function() {
+      const button = document.querySelector('.google-button');
+      button.classList.remove('active');
+    });
